8a66f93e6eda88ecb30787751089a1a7ebcf6986
Todd Stock <todd@avalabs.org>
2021-01-22 06:36:12 -0500

    upd

--- vms/avm/pubsub_parser.go
+++ vms/avm/pubsub_parser.go
@@ -14,26 +14,17 @@ func NewPubSubParser(tx *Tx) pubsub.Parser {
 }
 
 // Apply the filter on the addresses.
-// param is unchanged during filtering.
 func (p *parser) Filter(param *pubsub.FilterParam) *pubsub.FilterResponse {
 	for _, utxo := range p.tx.UTXOs() {
 		switch utxoOut := utxo.Out.(type) {
 		case avax.Addressable:
 			for _, address := range utxoOut.Addresses() {
-				response := p.filterParam(param, address)
-				if response != nil {
-					return response
+				if param.CheckAddress(address) {
+					return &pubsub.FilterResponse{TxID: p.tx.ID(), FilteredAddress: pubsub.ByteToID(address)}
 				}
 			}
 		default:
 		}
-	}
-	return nil
-}
-
-func (p *parser) filterParam(param *pubsub.FilterParam, sid []byte) *pubsub.FilterResponse {
-	if param.CheckAddress(sid) {
-		return &pubsub.FilterResponse{TxID: p.tx.ID(), FilteredAddress: pubsub.ByteToID(sid)}
 	}
 	return nil
 }
